f31f60a3462adc05e98da8218f7eac23684e455a
unknown <ebubeidika@gmail.com>
2024-02-10 23:11:18 +0100

    Added the screens folder

---
+++ screen/onboardingscreen.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function onboardingscreen() {
+  return (
+    <View>
+      <Text>onboardingscreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    
+})
