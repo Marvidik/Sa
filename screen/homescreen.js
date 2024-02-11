226efe316de642a6139f54bb04d482207256e4bb
unknown <ebubeidika@gmail.com>
2024-02-11 23:27:13 +0100

    home screen

---
+++ screen/homescreen.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function homescreen() {
+  return (
+    <View>
+      <Text>homescreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    
+})
