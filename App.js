5a507c62f5463df5b0aebaa4417eb97feb3d159b
unknown <ebubeidika@gmail.com>
2024-02-13 20:31:03 +0100

    a

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screen/loginscreen';
+import HomeScreen from './screen/homescreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Stated Off this App</Text>
-      <StatusBar style="auto" />
+      <HomeScreen/>
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
---
+++ screen/LoginScreen.js
@@ -0,0 +1,21 @@
+import { View, Text ,StyleSheet} from 'react-native'
+import React from 'react'
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>loginscreen</Text>
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+
+    container:{
+        paddingTop:44,
+
+    }
+    
+})
